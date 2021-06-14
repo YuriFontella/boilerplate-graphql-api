4c49060529d5ba6ac54e53a7354d1779b5e019f9
Yuri Fontella <yurifc4@gmail.com>
2021-06-14 15:42:01 -0300

    tools

--- src/graphql/tools/index.js
+++ src/graphql/tools/index.js
@@ -1,15 +1,12 @@
 'use strict'
 
 const { loadFilesSync } = require('@graphql-tools/load-files')
-
 const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge')
 
 const { resolve } = require('path')
 
 const typeDefs = mergeTypeDefs(loadFilesSync(resolve('src/graphql/schemas'), { extensions: ['graphql'] }))
-
 const resolvers = mergeResolvers(loadFilesSync(resolve('src/graphql/resolvers', '*.resolvers.*')))
-
 const loaders = mergeResolvers(loadFilesSync(resolve('src/graphql/loaders', '*.loaders.*')))
 
 module.exports = {
